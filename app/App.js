50b3b9845ea62373b40059b7a48f15eb414b8739
ErnaneOliveira <64278146+ErnaneOliveira@users.noreply.github.com>
2025-03-19 17:15:12 -0300

    Add files via upload

---
+++ app/App.js
@@ -0,0 +1,9 @@
+import MyCanvas from './src/app/VirtualKeyboard';
+import Home from './src/app/home';
+import Splash from './src/app/Splash';
+
+export default function App() {
+  return(
+  <Splash/>
+  )
+}
